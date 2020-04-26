c25f00ad15899f69b3f3674a3515302c3004b11d
Patrick Chen <52078333+patrickc-dev@users.noreply.github.com>
2020-04-26 13:24:28 +0100

    read data from master item json

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import fossils from './data/fossils.json';
-import villagers from './data/villagers.json';
+// import fossils from './data/fossils.json';
+// import villagers from './data/villagers.json';
+import itemsData from './data/items.json';
 
 
 
 function Bmc(props) {
   return (
     <div>
-      <a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/nooklist">
+      <a className="bmc-button" target="_blank" rel="noopener noreferrer" href="https://www.buymeacoffee.com/nooklist">
         <img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee"/>
         <span>Buy me a coffee</span>
       </a>
@@ -19,11 +20,14 @@ function Bmc(props) {
 
 
 function Item(props) {
+
+  const primaryImageUrl = props.item.variants[0].image
+
   return (
     <div className="card">
-      <div><img src={'https://acnhcdn.com/latest/' + props.item.imgurl + '.png'} alt="Avatar"></img></div>
-      <div>{props.item.en}</div>
-      <div>{props.item.zh}</div>
+      <div><img src={primaryImageUrl} alt=''></img></div>
+      <div>{props.item.name}</div>
+      <div>{props.item.name_zh}</div>
     </div>
   );
 }
@@ -37,22 +41,13 @@ function SearchBox(props) {
   );
 }
 
-function LangaugeSwitch(props) {
-  return (
-    <div>
-        <input type="checkbox" onChange={props.onChange}/>
-        中文 (現在沒用的按鈕)
-    </div>
-  );
-}
-
 function ItemType(props) {
   return (
     <div>
       Choose item type: 
       <select onChange={props.onChange} value={props.value}>
-        <option value="fossils">fossils</option>
-        <option value="villagers">villagers</option>
+        <option value="Housewares">家具</option>
+        <option value="Fossils">化石</option>
       </select>
     </div>
   );
@@ -64,10 +59,9 @@ class Main extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      items: fossils,
-      itemType: 'fossils',
+      items: itemsData.filter(item => (item.sourceSheet) === 'Fossils'),
+      itemType: 'Fossils',
       searchString: '',
-      language: 'en',
     };
   }
 
@@ -82,36 +76,26 @@ class Main extends React.Component {
     )
   }
 
-  languageHandler = (event) => {
-    console.log(event.target.checked);
-    if (event.target.checked) {
-      this.setState({language: 'zh'})
-    }
-    else {
-      this.setState({language: 'en'})
-    }
-  }
-
   itemTypeHandler = (event) => {
     console.log(event.target.value);
     const newItemType = event.target.value
-    if (newItemType == 'fossils') {
-      this.setState({items: fossils})
-    }
-    else {
-      this.setState({items: villagers})
-    }
+    const newItems = itemsData.filter(item => (item.sourceSheet) === event.target.value)
 
-    this.setState({itemType: newItemType})
+    this.setState({
+      items: newItems,
+      itemType: newItemType
+    })
 
   }
 
   render() {
+
     const filteredItems = this.state.items.filter(
-      item => (item.en + item.zh).includes(this.state.searchString)
+      item => (item.name).includes(this.state.searchString)
     )
+
     const items = filteredItems.map((item) => 
-        <li key={item.id}>
+        <li key={item.name}>
           <Item item={item}/>
         </li>
     );
@@ -119,10 +103,9 @@ class Main extends React.Component {
     return (
       <div>
         <Bmc/>
-        <LangaugeSwitch onChange={this.languageHandler}/>
         <SearchBox onChange={this.searchBoxHandler}/>
         <ItemType onChange={this.itemTypeHandler} value={this.state.itemType}/>
-        <ul class="cards">{items}</ul>
+        <ul className="cards">{items}</ul>
       </div>
     );
   }
