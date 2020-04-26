a5fe1635d44167e0c2a8a8e7650c2ae230400cd5
Patrick Chen <52078333+patrickc-dev@users.noreply.github.com>
2020-04-26 22:25:57 +0100

    add variant support

--- src/component/display/itemList.js
+++ src/component/display/itemList.js
@@ -1,47 +1,66 @@
 import './itemList.css';
-import React from 'react';
+import React, { Component } from 'react';
 
 function Variant(props) {
-    return (
-      <div className='variant'>
-        <img src={props.variant.image} alt=''></img>
-      </div>
-    );
+  return (
+    <div className='variant'>
+      <img 
+        src={props.variant.image} 
+        alt='' 
+        onClick={() => props.onClick(props.index)}
+        value='1'
+      ></img>
+    </div>
+  );
+}
+class Item extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      variant: 0
+    }
   }
-  
-  function Item(props) {
-  
-    const primaryImageUrl = props.item.variants[0].image
-    const variants = props.item.variants.map((variant) => 
+
+  variantClickHandler = (index) => {
+    // console.log(index)
+    this.setState({ variant: index});
+  }
+
+  render() { 
+    const primaryImageUrl = this.props.item.variants[this.state.variant].image
+
+    const variants = this.props.item.variants.map((variant, index) => 
       <div key={variant.uniqueEntryId} className='variant'>
-        <Variant variant={variant}/>
+        <Variant variant={variant} onClick={this.variantClickHandler} index={index}/>
       </div>
     );
   
     return (
       <div className="card">
         <div><img src={primaryImageUrl} alt=''></img></div>
-        <div>{props.item.name}</div>
-        <div>{props.item.name_zh}</div>
+        <div>{this.props.item.name}</div>
+        <div>{this.props.item.name_zh}</div>
         {variants.length>1 ? <div className='variants'>{variants}</div> : null}
       </div>
     );
   }
-  
+}
+
 function ItemList(props) {
-    const startItem = (props.pageNumber - 1) * 20
-    const displayItems = props.filteredItems.slice(startItem, startItem+20).map((item) => 
-        <div key={item.name}>
-          <Item item={item}/>
-        </div>
-    );
-  
-    return(
-      <div>
-        <div>{props.filteredItems.length + '個東西 顯示第' + (startItem+1) + ' - ' + (startItem+20) + '個'}</div>
-        <div className="cards">{displayItems}</div>
-      </div>
-    );
-  }
+  const startItem = (props.pageNumber - 1) * 20
+  const displayItems = props.filteredItems.slice(startItem, startItem+20).map((item) => 
+    <div key={item.name}>
+      <Item item={item}/>
+    </div>
+  );
+
+  return(
+    <div>
+      <div>{props.filteredItems.length + '個東西 顯示第' + (startItem+1) + ' - ' + (startItem+20) + '個'}</div>
+      <div className="cards">{displayItems}</div>
+    </div>
+  );
+}
 
 export default ItemList
--- src/component/monetize/bmc.js
+++ src/component/monetize/bmc.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 
 function Bmc(props) {
-    return (
-    <div>
-    <a className="bmc-button" target="_blank" rel="noopener noreferrer" href="https://www.buymeacoffee.com/nooklist">
-        <img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee"/>
-        <span>Buy me a coffee</span>
-    </a>
-    </div>
-    );
+  return (
+  <div>
+  <a className="bmc-button" target="_blank" rel="noopener noreferrer" href="https://www.buymeacoffee.com/nooklist">
+    <img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee"/>
+    <span>Buy me a coffee</span>
+  </a>
+  </div>
+  );
 }
 
 export default Bmc
